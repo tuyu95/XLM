7b635f75a846e41339dfb2bf2d7e2b2cdd2fdadb
Yu <43448591+tuyu95@users.noreply.github.com>
2019-06-07 01:33:32 +0100

    Update pretrain.sh

--- pretrain.sh
+++ pretrain.sh
@@ -5,7 +5,7 @@
 #SBATCH -N 1	  # nodes requested
 #SBATCH -n 1	  # tasks requested
 #SBATCH --partition=LongJobs
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:8
 #SBATCH --mem=12000  # memory in Mb
 #SBATCH --time=0-80:00:00
 
@@ -28,7 +28,7 @@ export PYTHON_PATH=$PATH
 
 cd /home/s1852803/unmt/XLM
 
-devices=0,1,2,3
+devices=0,1,2,3,4,5,6,7
 
 python train.py \
     --exp_name test_guen_mlm \
@@ -42,8 +42,8 @@ python train.py \
     --dropout 0.1 \
     --attention_dropout 0.1 \
     --gelu_activation true \
-    --batch_size 32 \
-    --bptt 256 \
+    --batch_size 16 \
+    --bptt 128 \
     --optimizer adam,lr=0.0001 \
     --epoch_size 200000 \
     --validation_metrics _valid_mlm_ppl \
