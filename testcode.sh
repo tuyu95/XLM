ce7af985532bbfea2706648c2e958b468ba0fbc4
Yu <43448591+tuyu95@users.noreply.github.com>
2019-06-12 21:09:13 +0100

    Update testcode.sh

--- testcode.sh
+++ testcode.sh
@@ -1,20 +1,29 @@
 #!/bin/sh
 
-python3 /home/s1852803/unmt/XLM/train.py \
-    --exp_name test_guen_mlm \
+CUDA_VISIBLE_DEVICES=$devices python3 /home/s1852803/unmt/XLM/train.py \
+    --exp_name unsupMT_guen \
     --dump_path /home/s1852803/unmt/XLM/dumped/ \
+    --reload_model '/home/s1852803/unmt/XLM/mlm_guen_ppl.pth, /home/s1852803/unmt/XLM/mlm_guen_ppl.pth' \
     --data_path /home/s1852803/unmt/XLM/data/processed/gu-en/ \
     --lgs 'gu-en' \
-    --clm_steps '' \
-    --mlm_steps 'gu,en' \
+    --ae_steps 'gu,en' \
+    --bt_steps 'gu-en-gu,en-gu-en' \
+    --word_shuffle 3 \
+    --word_dropout 0.1 \
+    --word_blank 0.1 \
+    --lambda_ae '0:1,100000:0.1,300000:0' \
+    --encoder_only false \
+    --emb_dim 1024 \
     --n_layers 6 \
     --n_heads 8 \
     --dropout 0.1 \
     --attention_dropout 0.1 \
     --gelu_activation true \
+    --tokens_per_batch 2000 \
     --batch_size 16 \
     --bptt 128 \
-    --optimizer adam,lr=0.0001 \
+    --optimizer adam_inverse_sqrt,beta1=0.9,beta2=0.98,lr=0.0001 \
     --epoch_size 200000 \
-    --validation_metrics _valid_mlm_ppl \
-    --stopping_criterion _valid_mlm_ppl,10
+    --eval_bleu true \
+    --stopping_criterion valid_gu-en_mt_bleu,10 \
+    --validation_metrics valid_gu-en_mt_bleu
