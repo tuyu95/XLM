1efb2d31e9caa1ca8ddf80519e6cd59b8991d8af
Yu <43448591+tuyu95@users.noreply.github.com>
2019-06-13 06:43:09 +0100

    Update train.sh

--- train.sh
+++ train.sh
@@ -25,14 +25,12 @@ export PYTHON_PATH=$PATH
 
 source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
 
-# devices=0,1,2,3
-
+devices=0,1,2,3
+# export NGPU=4
+# python -m torch.distributed.launch --nproc_per_node=$NGPU /home/s1852803/unmt/XLM/train.py \
 cd /home/s1852803/unmt/XLM
 
-
-
-export NGPU=4
-python -m torch.distributed.launch --nproc_per_node=$NGPU /home/s1852803/unmt/XLM/train.py \
+export CUDA_VISIBLE_DEVICES=$devices python3 /home/s1852803/unmt/XLM/train.py \
     --exp_name unsupMT_guen \
     --dump_path /home/s1852803/unmt/XLM/dumped/ \
     --reload_model /home/s1852803/unmt/XLM/mlm_guen_ppl.pth,/home/s1852803/unmt/XLM/mlm_guen_ppl.pth \
