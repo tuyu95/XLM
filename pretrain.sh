246b9d228dc2500e2cbff38e1aaef99353513f43
Yu <43448591+tuyu95@users.noreply.github.com>
2019-06-25 18:32:56 +0100

    Update pretrain.sh

--- pretrain.sh
+++ pretrain.sh
@@ -4,8 +4,8 @@
 #SBATCH -N 1	  # nodes requested
 #SBATCH -n 1	  # tasks requested
 #SBATCH --partition=General_Usage 
-#SBATCH --gres=gpu:8
-#SBATCH --mem=24000  # memory in Mb
+#SBATCH --gres=gpu:4
+#SBATCH --mem=12000  # memory in Mb
 #SBATCH --time=0-80:00:00
 
 export CUDA_HOME=/opt/cuda-9.0.176.1/
@@ -26,21 +26,21 @@ export PYTHON_PATH=$PATH
 
 source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
 
-devices=0,1,2,3,4,5,6,7
+devices=0,1,2,3
 
 CUDA_VISIBLE_DEVICES=$devices python3 /home/s1852803/unmt/XLM/train.py \
-    --exp_name test_engu_mlm \
+    --exp_name test_enfr_mlm \
     --dump_path /home/s1852803/unmt/XLM/dumped/ \
-    --data_path /home/s1852803/unmt/XLM/data/processed/en-gu/ \
-    --lgs 'en-gu' \
+    --data_path /home/s1852803/unmt/XLM/data/processed/en-fr/ \
+    --lgs 'en-fr' \
     --clm_steps '' \
-    --mlm_steps 'en,gu' \
+    --mlm_steps 'en,fr' \
     --n_layers 6 \
     --n_heads 8 \
     --dropout 0.1 \
     --attention_dropout 0.1 \
     --gelu_activation true \
-    --batch_size 32 \
+    --batch_size 16 \
     --bptt 128 \
     --optimizer adam,lr=0.0001 \
     --epoch_size 200000 \
