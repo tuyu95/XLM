7d3db075badcbd96de950b18f233f6a8b8c6d0e7
Yu <43448591+tuyu95@users.noreply.github.com>
2019-06-29 00:52:48 +0100

    Update get-data-para.sh

--- get-data-para.sh
+++ get-data-para.sh
@@ -240,14 +240,14 @@ split_data() {
         seed="$1"; openssl enc -aes-256-ctr -pass pass:"$seed" -nosalt </dev/zero 2>/dev/null
     };
     NLINES=`wc -l $1  | awk -F " " '{print $1}'`;
-    NTRAIN=$((NLINES - 5000));
-    NVAL=$((NTRAIN + 2500));
+    NTRAIN=$((NLINES - 10000));
+    NVAL=$((NTRAIN + 5000));
     echo $NLINES
     echo $NTRAIN
     echo $NVAL
     shuf --random-source=<(get_seeded_random 42) $1 | head -$NTRAIN             > $2;
-    shuf --random-source=<(get_seeded_random 42) $1 | head -$NVAL | tail -2500  > $3;
-    shuf --random-source=<(get_seeded_random 42) $1 | tail -2500                > $4;
+    shuf --random-source=<(get_seeded_random 42) $1 | head -$NVAL | tail -5000  > $3;
+    shuf --random-source=<(get_seeded_random 42) $1 | tail -5000                > $4;
 }
 for lg in $(echo $pair | sed -e 's/\-/ /g'); do
   split_data $PARA_PATH/$pair.$lg.all $PARA_PATH/$pair.$lg.train $PARA_PATH/$pair.$lg.valid $PARA_PATH/$pair.$lg.test
