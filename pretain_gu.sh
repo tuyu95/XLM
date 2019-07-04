3e0fc8ef1bfc610261eb9279fa4f3004d024dff1
Yu <43448591+tuyu95@users.noreply.github.com>
2019-07-04 23:41:33 +0100

    Create pretain_gu.sh

---
+++ pretain_gu.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+
+#SBATCH -N 1	  # nodes requested
+#SBATCH -n 1	  # tasks requested
+#SBATCH --partition=Standard 
+#SBATCH --gres=gpu:2
+#SBATCH --mem=12000  # memory in Mb
+#SBATCH --time=0-08:00:00
+
+export CUDA_HOME=/opt/cuda-9.0.176.1/
+
+export CUDNN_HOME=/opt/cuDNN-7.0/
+
+export STUDENT_ID=$(whoami)
+
+export LD_LIBRARY_PATH=${CUDNN_HOME}/lib64:${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
+
+export LIBRARY_PATH=${CUDNN_HOME}/lib64:$LIBRARY_PATH
+
+export CPATH=${CUDNN_HOME}/include:$CPATH
+
+export PATH=${CUDA_HOME}/bin:${PATH}
+
+export PYTHON_PATH=$PATH
+
+source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
+
+devices=0,1
+
+CUDA_VISIBLE_DEVICES=$devices python3 /home/s1852803/unmt/XLM/train.py \
+    --exp_name test_engu_mlm_1024 \
+    --dump_path /home/s1852803/unmt/XLM/dumped/ \
+    --data_path /home/s1852803/unmt/XLM/data/processed/en-gu/ \
+    --lgs 'en-gu' \
+    --clm_steps '' \
+    --mlm_steps 'en,gu' \
+    --emb_dim 1024 \
+    --n_layers 6 \
+    --n_heads 8 \
+    --dropout 0.1 \
+    --attention_dropout 0.1 \
+    --gelu_activation true \
+    --batch_size 16 \
+    --bptt 128 \
+    --optimizer adam,lr=0.0001 \
+    --epoch_size 200000 \
+    --eval_bleu true \
+    --validation_metrics _valid_mlm_ppl \
+    --stopping_criterion _valid_mlm_ppl,10
