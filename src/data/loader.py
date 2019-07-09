7f22f3fc3b177e1d2ac05a131822d1a1b51e4a6e
Yu <43448591+tuyu95@users.noreply.github.com>
2019-07-09 22:03:19 +0100

    Update loader.py

--- src/data/loader.py
+++ src/data/loader.py
@@ -308,11 +308,11 @@ def check_data_params(params):
     }
 #    assert all([all([os.path.isfile(p1) and os.path.isfile(p2) for p1, p2 in paths.values()]) for paths in params.para_dataset.values()])
     for paths in params.para_dataset.values():
-    for p1, p2 in paths.values()
-        if not os.path.isfile(p1):
-            print(p1)
-        if not os.path.isfile(p2):
-            print(p2)
+        for p1, p2 in paths.values()
+            if not os.path.isfile(p1):
+                print(p1)
+            if not os.path.isfile(p2):
+                print(p2)
             
     # check that we can evaluate on BLEU
     assert params.eval_bleu is False or len(params.mt_steps + params.bt_steps) > 0
